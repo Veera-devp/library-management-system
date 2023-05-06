1fe0c886f708cfbd31123676b41c0a56b32f7889
Veera Raghava Sai Varma <82495769+Veera-devp@users.noreply.github.com>
2023-05-06 23:07:18 +0530

    Add files via upload

---
+++ src/main/java/com/libproject/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,17 @@
+package com.libproject.springbootlibrary.dao;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.libproject.springbootlibrary.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book,Long>
+{
+	Page<Book> findByTitleContaining(@RequestParam("title") String title,Pageable pageable);
+	Page<Book> findByCategory(@RequestParam("category") String category,Pageable pageable);
+}
+

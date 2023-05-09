0e58b352fe90b82e83e8a36e573826ca8a2da08a
Veera Raghava Sai Varma <82495769+Veera-devp@users.noreply.github.com>
2023-05-09 08:24:53 +0530

    Add files via upload

---
+++ src/main/java/com/libproject/springbootlibrary/controller/BookController.java
@@ -0,0 +1,46 @@
+package com.libproject.springbootlibrary.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libproject.springbootlibrary.entity.Book;
+import com.libproject.springbootlibrary.service.BookService;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+    
+    @Autowired
+    public BookController(BookService bookService)
+    {
+    	this.bookService = bookService;
+    }
+    @GetMapping("secure/currentloans/count")
+    public int currentLoansCount()
+    {
+    	String userEmail = "testuser@email.com";
+    	return bookService.currentLoansCount(userEmail);
+    }
+    @GetMapping("secure/ischeckedout/byuser")
+    public Boolean checkoutBookByUser(@RequestParam Long bookId)
+    {
+    	String userEmail="testuser@email.com";
+    	return bookService.checkoutBookByUser(userEmail, bookId);
+    }
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception
+    {
+    	String userEmail = "testuser@email.com";
+    	return bookService.checkoutBook(userEmail,bookId);
+    }
+    
+
+}
--- src/main/java/com/libproject/springbootlibrary/dao/CheckoutRepository.java
+++ src/main/java/com/libproject/springbootlibrary/dao/CheckoutRepository.java
@@ -1,9 +1,13 @@
 package com.libproject.springbootlibrary.dao;
 
-import com.libproject.springbootlibrary.entity.Checkout;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.libproject.springbootlibrary.entity.Checkout;
 public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
 
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
+    
+    List<Checkout> findBooksByUserEmail(String userEmail);
 }
